3fc4a827e761d691da2e9b6709360120cb5354ad
PhuNguyen <157004724+NguyenTrungPhuIvan@users.noreply.github.com>
2024-04-30 16:47:21 +0700

    fix switch in Choice

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
 var $ = document.querySelector.bind(document);
 var $$ = document.querySelectorAll.bind(document);
 
-var tabs = $$(".vacation__list-title");
-var panes = $$(".vacation__list-desc");
-var line = $(".vacation__separate");
+function start() {
+    tabUI();
+}
 
-var tabs_first_active = $(".vacation__list-title.vacation__list-title-active");
-console.log(tabs);
+start();
 
-line.style.left = tabs_first_active.offsetLeft + "px";
-line.style.width = tabs_first_active.offsetWidth + "px";
+function tabUI() {
+    var tabs = $$(".vacation__list-title");
+    var panes = $$(".vacation__list-desc");
+    var line = $(".vacation__separate");
 
-tabs.forEach((tab, index) => {
-    tab.onclick = function () {
-        var pane = panes[index];
-        $(".vacation__list-desc.vacation__list-desc-active").classList.remove(
-            "vacation__list-desc-active"
-        );
-        pane.classList.add("vacation__list-desc-active");
+    var tabs_first_active = $(
+        ".vacation__list-title.vacation__list-title-active"
+    );
+    console.log(tabs);
 
-        $(".vacation__list-title.vacation__list-title-active").classList.remove(
-            "vacation__list-title-active"
-        );
-        tab.classList.add("vacation__list-title-active");
-        line.style.left = tab.offsetLeft + "px";
-        line.style.width = tab.offsetWidth + "px";
-    };
-});
+    line.style.left = tabs_first_active.offsetLeft + "px";
+    line.style.width = tabs_first_active.offsetWidth + "px";
+
+    tabs.forEach((tab, index) => {
+        tab.onclick = function () {
+            var pane = panes[index];
+            $(
+                ".vacation__list-desc.vacation__list-desc-active"
+            ).classList.remove("vacation__list-desc-active");
+            pane.classList.add("vacation__list-desc-active");
+
+            $(
+                ".vacation__list-title.vacation__list-title-active"
+            ).classList.remove("vacation__list-title-active");
+            tab.classList.add("vacation__list-title-active");
+            line.style.left = tab.offsetLeft + "px";
+            line.style.width = tab.offsetWidth + "px";
+        };
+    });
+}
